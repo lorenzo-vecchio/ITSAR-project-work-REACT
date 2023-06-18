901a5feeb0577b07ecd2de4b2767c64e948a6079
daniele-rizzi <daniele.rizzi98@hotmail.com>
2023-06-18 22:16:55 +0200

    .

--- src/components/AddAnimalForm.jsx
+++ src/components/AddAnimalForm.jsx
@@ -2,8 +2,11 @@ import React, {useState} from 'react';
 import '../css/AddAnimalForm.css';
 import FotoWidget from "../components/FotoWidget";
 import Image from "../placeholders/dog.jpg";
+import Input from "@mui/material/Input";
+import InputAdornment from "@mui/material/InputAdornment"; //to install it, write the command: npm install @mui/material @emotion/react; npm install @emotion/styled @mui/lab @mui/icons-material
 
 const AddAnimalForm = () => {
+    const buttonSend = document.getElementById('buttonAggiungi');
     const [checkbox1Checked, setCheckbox1Checked] = useState(false);
     const [checkbox2Checked, setCheckbox2Checked] = useState(false);
     const [isHover, setIsHover] = useState(false);
@@ -30,25 +33,42 @@ const AddAnimalForm = () => {
         }
     };
 
+    const [formData, setFormData] = useState({
+        Nome: '',
+        Razza: '',
+        Specie: '',
+        Genere: '',
+        DataDiNascita: '',
+        Peso: '',
+      });
 
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const jsonData = JSON.stringify(formData);
+        console.log(jsonData);
+        };
+      
     return(
         
-        <form method="get" className="style">
+        <form className="style" onSubmit={handleSubmit}>
             <div></div>
             <FotoWidget image={Image} />
 
                 <label className="label">Nome: </label>
-                <input type="text" className="input" />
-
+                <input type="text" className="input" name="Nome" onChange={handleChange}/> 
                 <label className="label">Specie: </label>   
-                <input type="text" list="specie" className="input" id="inputDataList1"/>
+                <input type="text" list="specie" name="Specie" className="input" id="inputDataList1" onChange={handleChange}/>
                 <datalist id="specie">
                     <option value="Cane" />
                     <option value="Gatto" />
                 </datalist>
 
                 <label className="label">Razza: </label>
-                <input type="text" list="razza" className="input" id="inputDataList2"/>
+                <input type="text" list="razza" name="Razza" className="input" id="inputDataList2" onChange={handleChange}/>
                 <datalist id="razza">
                     <option value="Labrador" />
                     <option value="Persiano" />
@@ -56,28 +76,44 @@ const AddAnimalForm = () => {
                 <div>
                     {/*vuoto per griglia*/}
                 </div>
+
                 <div className="checkboxList">
                     <label>M</label>
-                    <input type="checkbox" id="boxStyle1" checked={checkbox1Checked}
-                    onChange={handleCheckbox1Change}/>
+                    <input type="checkbox" id="boxStyle1" checked={checkbox1Checked}/>
                     <label>F</label>
-                    <input type="checkbox" id="boxStyle2" checked={checkbox2Checked}
-                    onChange={handleCheckbox2Change}/>
+                    <input type="checkbox" id="boxStyle2" checked={checkbox2Checked}/>
                 </div>
 
-
                 <label className="label">Data di nascita: </label>
-                <input type="date" className="inputDate" />
+                <input type="date" name="DataDiNascita" onChange={handleChange} className="inputDate"/>
 
                 <label className="label">Peso: </label>
-                <input  type="number" className="input" placeholder="0 Kg" min="0"/>
+                <input
+                    type="number" 
+                    id="kginput" 
+                    className="input"
+                    placeholder="0 Kg" 
+                    min="0"
+                    name="Peso"
+                    onChange={handleChange}
+                />
 
-            {/*
-            <div>
-                <label style={style.label}>Vaccinazioni: </label>
-                <textarea type="text" style={style.vaccinazioni} placeholder="Inserire le vaccinazioni"/>
-            </div>
-    */}
+                {/*
+                <div>
+                    <label style={style.label}>Vaccinazioni: </label>
+                    <textarea type="text" style={style.vaccinazioni} placeholder="Inserire le vaccinazioni"/>
+                </div>
+                */}         
+
+                <div>
+                    {/*vuoto per griglia*/}
+                </div>
+
+                <div className="buttonsBottomForm">
+                    <button type="reset" className="buttonForm" id="buttonAnnulla">Annulla</button>
+                    <button type="submit" className="buttonForm" id="buttonAggiungi" onClick={handleSubmit}>Aggiungi</button>
+                </div>
+
         </form>
 
 
