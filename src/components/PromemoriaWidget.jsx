f8c9b3fa7a35ee529b033f02079706114d41a4f9
lorenzo-vecchio <106695602+lorenzo-vecchio@users.noreply.github.com>
2023-06-27 11:57:34 +0200

    connected api to promemoria

--- src/components/PromemoriaWidget.jsx
+++ src/components/PromemoriaWidget.jsx
@@ -3,14 +3,31 @@ import BoxPromemoria from './BoxPromemoria';
 import {AggiungiPromemoria} from './Aggiungi';
 import ImmagineAggiungi from "../media/add.svg"
 import ImmagineRemove from "../media/remove.svg"
+import { useState, useEffect } from 'react';
 
 const PromemoriaWidget = () => {
+    const [promemoria, setPromemoria] = useState([]);
+    const requestOptions = {
+        credentials: 'include',
+    }
+    useEffect(() => {
+        fetch("https://itsar-project-work-api.vercel.app/promemoria", requestOptions)
+        .then(res => res.json())
+        .then(
+        (result) => {
+            setPromemoria(result);
+        })
+    }, [])
     return (
         <div style={styles.container}>
             <h1>I tuoi Promemoria</h1>
-            <BoxPromemoria title={"veterinaio"} descrizione={"devo portare il mio cane dal veterinaio"} data={"14/06/2023"} animale={"Fido"} ora={"8:30"} img={ImmagineRemove} aImg={10} lImg={10}/>
-            <BoxPromemoria title={"veterinaio"} descrizione={"devo portare il mio cane dal veterinaio"} data={"14/06/2023"} animale={"Fido"} ora={"8:30"} img={ImmagineRemove} aImg={10} lImg={10}/>
-            <BoxPromemoria title={"veterinaio"} descrizione={"devo portare il mio cane dal veterinaio"} data={"14/06/2023"} animale={"Fido"} ora={"8:30"} img={ImmagineRemove} aImg={10} lImg={10}/>
+            {
+                promemoria?.map((item) => {
+                    return (
+                        <BoxPromemoria title={item.titolo} descrizione={item.descrizione} data={item.data_ora.split('T')[0]} animale={item.animali} ora={item.data_ora.split('T')[1]} img={ImmagineRemove} aImg={10} lImg={10}/>
+                    )
+                })
+            }
             <AggiungiPromemoria descrizione={"aggiungi promemoria"} lunghezza={"80"} colore={"green"} img={ImmagineAggiungi}/>
         </div>
     );
