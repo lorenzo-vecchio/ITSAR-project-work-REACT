3a1adb66a4c341f913853c47c75a1dc9e52f7155
daniele-rizzi <133057546+daniele-rizzi@users.noreply.github.com>
2023-06-30 10:46:00 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,7 @@ import Background from "./components/Background";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import MapPage from "./pages/MapPage";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import AddAnimalPage from "./pages/AddAnimalPage";
-=======
->>>>>>> master
-=======
-import AddAnimalPage from "./pages/AddAnimalPage";
->>>>>>> b2aca668513959c87b48dd0b0eded9f7102fb456
 import UserPage from "./pages/UserPage";
 
 export default function App() {
--- src/components/FotoWidget.jsx
+++ src/components/FotoWidget.jsx
@@ -3,15 +3,9 @@ import React from "react";
 const FotoWidget = (props) =>{
     return (
         <div>
-<<<<<<< HEAD
-<<<<<<< HEAD
             <img style={styles.immagine} src={props.image} alt=""/>
-=======
             <img style={styles.immagine} src={props.image} />
->>>>>>> master
-=======
             <img style={styles.immagine} src={props.image} alt=""/>
->>>>>>> b2aca668513959c87b48dd0b0eded9f7102fb456
         </div>
     )
 }
