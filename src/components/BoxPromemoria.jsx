d0ea1ded8e8402c2a49f8022241ce325e7df0b84
daniele-rizzi <133057546+daniele-rizzi@users.noreply.github.com>
2023-06-30 11:53:57 +0200

    modifiche promemoria

--- src/components/BoxPromemoria.jsx
+++ src/components/BoxPromemoria.jsx
@@ -5,14 +5,13 @@ const BoxPromemoria = ({title, descrizione, data, animale, ora, img, lImg, aImg}
     return(
         <div className='boxProm'>
             <div className='barraTitolo'>
-                <p className='tdoProm'>{title}</p>
                 <p className='tdoProm'>{data}</p>
                 <p className='tdoProm'>{ora}</p>
                 <img src={img} width={lImg} height={aImg}/>
             </div>
             <div className='descrizioneProm'>
+            <span className='paragDescProm'><strong>Animale:</strong> {animale}</span>
                 <span className='paragDescProm'><strong>Descrizione:</strong> {descrizione}</span>
-                <span className='paragDescProm'><strong>Animale:</strong> {animale}</span>
             </div>
         </div>
     )
